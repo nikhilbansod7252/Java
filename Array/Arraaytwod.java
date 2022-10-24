48877836502dc06f007682bb78e29067434403a6
Nikhil Bansod <nikhilb7252@gmail.com>
2022-10-24 16:04:14 +0530

    Add files via upload

---
+++ Array/Arraaytwod.java
@@ -0,0 +1,32 @@
+package Day4;
+
+public class Arraaytwod {
+
+	public static void main(String[] args) {
+		
+//		int[][] matrix = {{2,3},{4,5}};
+//		System.out.println(matrix[0][0]);
+//		System.out.println(matrix[0][1]);
+//		System.out.println(matrix[1][0]);
+//		System.out.println(matrix[1][1]);
+				
+//		System.out.println(matrix[0][0] +" "+ matrix[0][1]);
+//		System.out.println(matrix[1][0] +" "+ matrix[1][1]);
+		
+//		int[][] matrix = new int[2][2];
+//		matrix[0][0] = 2;
+//		matrix[0][1] = 3;
+//		matrix[1][0] = 4;
+//		matrix[1][1] = 5;
+//		System.out.println(matrix[1][0]);
+		
+		//Another way of creating and initializing 2D array 
+//		int[][] matrix = new int[][] {{2,3},{4,5}};
+		
+		
+		
+		
+
+	}
+
+}
