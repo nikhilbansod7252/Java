cffbacde53591362be13cca8e521207f9d563a7c
Nikhil Bansod <nikhilb7252@gmail.com>
2022-11-17 07:08:08 +0530

    Add files via upload

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,68 @@
+package Patterns;
+//*
+//**
+//***
+//****
+//public class Pattern1 {
+//outerloop for row travel and inner loop for column travel
+//	public static void main(String[] args) {
+//		for( int row = 1; row <= 4; row++ ) {
+//			for(int col = 1; col <= row; col++) {
+//				System.out.print("*");
+//			}`
+//			System.out.println();
+//		}
+//	}
+//}
+
+//****
+//***
+//**
+//*
+//public class Pattern1 {
+//	public static void main(String[] args) {
+//		for(int row = 1; row <= 4; row++) {
+//			for( int col = 4; col >= row; col--) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+//1
+//12
+//123
+//1234
+//public class Pattern1 {
+//public static void main(String[] args) {
+//	for(int row = 1; row <= 4; row++) {
+//		for(int col = 1; col <= row; col++) {
+//			System.out.print(col);
+//		}
+//		System.out.println();
+//	}
+//  }
+//}
+
+//1234
+//123
+//12
+//1
+//public class Pattern1 {
+//	public static void main(String[] args) {
+//		for(int row = 4; row >= 1; row--) {
+//			for(int col = 1; col <= row; col++) {
+//				System.out.print(col);
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+
+
+
+
+
+
+
